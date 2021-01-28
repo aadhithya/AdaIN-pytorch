8ed34fea143933ba776801f01ff43cd359d65567
Aadhithya Sankar <a.sankar@tum.de>
2021-01-28 23:26:52 +0100

    WIP main CLI

---
+++ main.py
@@ -0,0 +1,15 @@
+from typer import Typer
+
+from train import Trainer
+
+app = Typer(name="AdaIN Style Transfer")
+
+
+@app.command("train")
+def train():
+    pass
+
+
+@app.command("infer")
+def infer():
+    pass
