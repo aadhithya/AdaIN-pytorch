0da3fb959ffe152edd99ceac62c7e2d0aae075d4
Aadhithya Sankar <a.sankar@tum.de>
2021-01-30 03:43:16 +0100

    minor updates

--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@ def inv_normz(img):
     mean = (
         torch.Tensor([0.485, 0.456, 0.406]).reshape(-1, 1, 1).to(img.device)
     )
-    res = torch.clamp(img * std + mean, 0, 1)
-    return res
+    out = torch.clamp(img * std + mean, 0, 1)
+    return out
 
 
 def img_loader(path: str):
