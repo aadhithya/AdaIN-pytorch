07f4456743153fdf5282245ed5ad7689fad55af9
Aadhithya Sankar <a.sankar@tum.de>
2021-04-02 13:24:00 +0200

    Revert "test""
    This reverts commit 57ad8a1129c025d9f463e51656c535b87af3342a.

--- train.py
+++ train.py
@@ -352,7 +352,3 @@ class SRTrainer(Trainer):
             if (ix + 1) % self.ckpt_freq == 0:
                 self.save_model_weights()
             self.train_step += 1
-
-class Train2(Trainer):
-    def __init__(self, content_dir: str, style_dir: str, num_iters: int, n_epochs: int, imsize: int, lr: float, batch_size: int, wt_s: float, num_samples: int, ckpt_freq: int, seed: int, ckpt_path: Optional[str], device: str) -> None:
-        super().__init__(content_dir, style_dir, num_iters=num_iters, n_epochs=n_epochs, imsize=imsize, lr=lr, batch_size=batch_size, wt_s=wt_s, num_samples=num_samples, ckpt_freq=ckpt_freq, seed=seed, ckpt_path=ckpt_path, device=device)
